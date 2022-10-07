64f0062a50bc4c3c329e1cdef11b357b24d8823d
Oleg Palashynskyi <palah@ukr.net>
2022-10-07 15:41:20 +0300

    123

--- src/components/Statistics/MainStatistics.jsx
+++ src/components/Statistics/MainStatistics.jsx
@@ -1,58 +1,80 @@
-import React, { Component } from "react";
-import StatCard from "./StatitisticCards";
-import css from './StatisticsStyles.module.css'
+import React, { Component } from 'react';
+import StatCard from './StatitisticCards';
+import css from './StatisticsStyles.module.css';
 
 class StatCompon extends Component {
-
   constructor() {
     super();
     this.state = {
-      statsElements : [
-      { id:1,
-        title: "Likes",
-        count: 1000,
-      },
-      { id:2,
-        title: "Members",
-        count: 1000,
-      },
-      { id:3,
-        title: "Products",
-        count: 1000,
-      },
-      { id:4,
-        title: "Trees",
-        count: 1000,
-      },
-      ]
-    }
+      statsElements: [
+        { id: 1, title: 'Likes', count: 1000 },
+        { id: 2, title: 'Members', count: 1000 },
+        { id: 3, title: 'Products', count: 1000 },
+        { id: 4, title: 'Trees', count: 1000 },
+      ],
+    };
   }
 
-  hendelCounter = (id) => {
-    
+  hendelCounter = id => {
     const newElArr = this.state.statsElements.map(el => {
       if (el.id === id) {
-          return { ...el, count:el.count += 1 }
-      } 
-      return el
-    })
-    console.log(newElArr)
-    this.setState({ statsElements :newElArr})
-  }
+        return { ...el, count: (el.count += 1) };
+      }
+      return el;
+    });
+    this.setState({ statsElements: newElArr });
+  };
+
+  hendelDecreaseCounter = id => () => {
+    const { statsElements } = this.state;
+    const x = statsElements.find(el => el.id === id);
+    console.log(x);
+    if (!x.count) return;
+
+    const newElArr = this.state.statsElements.map(el => {
+      if (el.id === id) {
+        return { ...el, count: (el.count -= 1) };
+      }
+      return el;
+    });
+    this.setState({ statsElements: newElArr });
+  };
+
+  hendelResetCounter = id => () => {
+    const newElArr = this.state.statsElements.map(el => {
+      if (el.id === id) {
+        return { ...el, count: 0 };
+      }
+      return el;
+    });
+    this.setState({ statsElements: newElArr });
+  };
+
+  hendelDeleteCard = id => {
+    const filteredCards = this.state.statsElements.filter(el => {
+      if (el.id === id) {
+        return false;
+      }
+      return el;
+    });
+    this.setState({ statsElements: filteredCards });
+  };
 
   render() {
-  
     return (
       <div className={css.statistics_wrapper}>
         <h1 className={css.statistics_header}>Main statistics</h1>
         <div className={css.statistics_cards_wrapper}>
-          {this.state.statsElements.map(({id,title,count}) => (
+          {this.state.statsElements.map(({ id, title, count }) => (
             <StatCard
               id={id}
               title={title}
               count={count}
               key={id}
-              hendlClick = {this.hendelCounter}
+              hendlClickPlus={this.hendelCounter}
+              hendelDeleteCard={this.hendelDeleteCard}
+              hendlClickMinuse={this.hendelDecreaseCounter(id)}
+              hendelResetCounter={this.hendelResetCounter(id)}
             />
           ))}
         </div>
--- src/components/Statistics/StatitisticCards.jsx
+++ src/components/Statistics/StatitisticCards.jsx
@@ -1,21 +1,46 @@
-import React from "react";
+import React from 'react';
 import css from './StatisticsStyles.module.css';
 import { ReactComponent as UpIcon } from 'static/img/up.svg';
 import { ReactComponent as CrossIcon } from 'static/img/cross.svg';
 import { ReactComponent as DownIcon } from 'static/img/down.svg';
 import { ReactComponent as TrashIcon } from 'static/img/thrash.svg';
 
-const StatCard = ({id,title,count,hendlClick}) => {
-    return <div className={css.card_wrapper} onClick={() => hendlClick(id)}>
-        <div className={css.buttons}>
-            <button className={css.increase_button}><UpIcon className={css.buttonIcon} /></button>
-            <button className={css.clear_button}><CrossIcon className={css.buttonIcon} /></button>
-            <button className={css.decrease_button}><DownIcon className={css.buttonIcon} /></button>
-        </div>
-        <span className={css.count_value}> {count}</span>
-        <span className={css.card_description}>{title}</span>
-        <button className={css.delete_button}>Delete card<TrashIcon className={css.thrashIcon} /></button>
+const StatCard = ({
+  id,
+  title,
+  count,
+  hendlClickPlus,
+  hendlClickMinuse,
+  hendelResetCounter,
+  hendelDeleteCard,
+}) => {
+  const hendlUpClick = () => hendlClickPlus(id);
+  const hendelDeleteClick = () => hendelDeleteCard(id);
+
+  return (
+    <div className={css.card_wrapper}>
+      <div className={css.buttons}>
+        <button
+          className={css.increase_button}
+          onClick={() => hendlUpClick(id)}
+        >
+          <UpIcon className={css.buttonIcon} />
+        </button>
+        <button className={css.clear_button}>
+          <CrossIcon className={css.buttonIcon} onClick={hendelResetCounter} />
+        </button>
+        <button className={css.decrease_button}>
+          <DownIcon className={css.buttonIcon} onClick={hendlClickMinuse} />
+        </button>
+      </div>
+      <span className={css.count_value}> {count}</span>
+      <span className={css.card_description}>{title}</span>
+      <button className={css.delete_button} onClick={hendelDeleteClick}>
+        Delete card
+        <TrashIcon className={css.thrashIcon} />
+      </button>
     </div>
-}
+  );
+};
 
 export default StatCard;
