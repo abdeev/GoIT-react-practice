b348ca1a30306127d1050fd1cfab620e3d558e34
feninTolia <fenintolja@gmail.com>
2022-10-07 15:14:36 +0300

    add handlers

--- src/components/Statistics/MainStatistics.jsx
+++ src/components/Statistics/MainStatistics.jsx
@@ -1,58 +1,74 @@
-import React, { Component } from "react";
-import StatCard from "./StatitisticCards";
-import css from './StatisticsStyles.module.css'
+import React, { Component } from 'react';
+import StatCard from './StatitisticCards';
+import css from './StatisticsStyles.module.css';
 
 class StatCompon extends Component {
-
   constructor() {
     super();
     this.state = {
-      statsElements : [
-      { id:1,
-        title: "Likes",
-        count: 1000,
-      },
-      { id:2,
-        title: "Members",
-        count: 1000,
-      },
-      { id:3,
-        title: "Products",
-        count: 1000,
-      },
-      { id:4,
-        title: "Trees",
-        count: 1000,
-      },
-      ]
-    }
+      statsElements: [
+        { id: 1, title: 'Likes', count: 1000 },
+        { id: 2, title: 'Members', count: 1000 },
+        { id: 3, title: 'Products', count: 1000 },
+        { id: 4, title: 'Trees', count: 1000 },
+      ],
+    };
   }
 
-  hendelCounter = (id) => {
-    
+  hendelIncreaseCounter = id => {
     const newElArr = this.state.statsElements.map(el => {
       if (el.id === id) {
-          return { ...el, count:el.count += 1 }
-      } 
-      return el
-    })
-    console.log(newElArr)
-    this.setState({ statsElements :newElArr})
-  }
+        return { ...el, count: (el.count += 1) };
+      }
+      return el;
+    });
+    this.setState({ statsElements: newElArr });
+  };
+
+  handleDecreaseCounter = id => () => {
+    const { statsElements } = this.state;
+
+    const x = statsElements.find(el => el.id === id);
+
+    if (!x.count) {
+      return;
+    }
+
+    const newElArr = this.state.statsElements.map(el => {
+      if (el.id === id) {
+        return { ...el, count: (el.count -= 1) };
+      }
+      return el;
+    });
+    this.setState({ statsElements: newElArr });
+  };
+
+  handleResetCounter = id => {
+    return () => {
+      const newElArr = this.state.statsElements.map(el => {
+        if (el.id === id) {
+          return { ...el, count: 0 };
+        }
+        return el;
+      });
+      this.setState({ statsElements: newElArr });
+    };
+  };
 
   render() {
-  
     return (
       <div className={css.statistics_wrapper}>
         <h1 className={css.statistics_header}>Main statistics</h1>
         <div className={css.statistics_cards_wrapper}>
-          {this.state.statsElements.map(({id,title,count}) => (
+          {this.state.statsElements.map(({ id, title, count }) => (
             <StatCard
               id={id}
               title={title}
               count={count}
               key={id}
-              hendlClick = {this.hendelCounter}
+              hendlIncreaseClick={this.hendelIncreaseCounter}
+              handleDecreaseCounter={this.handleDecreaseCounter(id)}
+              handleResetCounter={this.handleResetCounter(id)}
             />
           ))}
         </div>
